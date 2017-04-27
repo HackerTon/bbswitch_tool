a6b5168f04f89484174703e347ed6e241062a645
hackerton <alanlamkenlun@hotmail.com>
2017-04-27 18:19:36 +0800

    Fix script BUG

--- bbswitch.sh
+++ bbswitch.sh
@@ -5,15 +5,18 @@ BBSWITCH_LOC=/proc/acpi/bbswitch
 
 if [ -f $BBSWITCH_LOC ]; then
 	if grep ON $BBSWITCH_LOC >> /dev/null ; then
-		echo "Turning OFF GPU"
+		echo "Turning OFF GPU"	
 		sudo rmmod nvidia_uvm
 		sudo rmmod nvidia_modeset
 		sudo rmmod nvidia
-		sudo tee $BBSWITCH_LOC <<<OFF
+		sudo tee $BBSWITCH_LOC <<<OFF	
 	else
 		echo "Turning ON GPU"
-		sudo modprobe nvidia
 		sudo tee $BBSWITCH_LOC <<<ON
+		sudo modprobe nvidia
+		sudo modprobe nvidia_modeset
+		sudo modprobe nvidia_uvm
+	
 	fi
 else
 	echo "CANNOT FIND "$BBSWTICH_LOC
